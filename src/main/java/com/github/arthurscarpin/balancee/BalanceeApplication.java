5376458a983764fe023c636a774db758aa2a41e0
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-13 20:50:13 -0300

    chore: initial commit

---
+++ src/main/java/com/github/arthurscarpin/balancee/BalanceeApplication.java
@@ -0,0 +1,13 @@
+package com.github.arthurscarpin.balancee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/arthurscarpin/balancee/BalanceeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arthurscarpin.balancee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
