58fc1f0d8e045bc374b7acd89f23a9f8382b71f4
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-15 15:09:49 -0300

    fix: Adjust the return types of assign methods to use Optional

--- src/main/java/com/github/arthurscarpin/balancee/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/com/github/arthurscarpin/balancee/domain/transaction/repository/TransactionRepository.java
@@ -6,15 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     @Query("SELECT * FROM Transaction t WHERE t.user.id = :userId")
-    List<Transaction> findByUserId(@Param("userId") Long userId);
+    Optional<List<Transaction>> findByUserId(@Param("userId") Long userId);
 
     @Query ("SELECT * FROM Transaction t WHERE t.user.id = :userId AND t.type = :type")
-    List<Transaction> findByType(@Param("userId") Long userId, @Param("type") String type);
+    Optional<List<Transaction>> findByType(@Param("userId") Long userId, @Param("type") String type);
 
     @Query("SELECT * FROM Transaction t WHERE t.user.id = :userId AND YEAR(t.date) = :year AND MONTH(t.date) = :month")
-    List<Transaction> findByYearAndMonth(@Param("userId") Long userId, @Param("year") int year, @Param("month") int month);
+    Optional<List<Transaction>> findByYearAndMonth(@Param("userId") Long userId, @Param("year") int year, @Param("month") int month);
 }
