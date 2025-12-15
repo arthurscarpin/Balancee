c7e609c36fe6d3f7dd7b2bc2fb40f33745fa08ae
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-14 23:08:06 -0300

    feat: Add the custom queries to retrieve transactions based on user, type, and date
    Closes #8

--- src/main/java/com/github/arthurscarpin/balancee/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/com/github/arthurscarpin/balancee/domain/transaction/repository/TransactionRepository.java
@@ -2,6 +2,19 @@ package com.github.arthurscarpin.balancee.domain.transaction.repository;
 
 import com.github.arthurscarpin.balancee.domain.transaction.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("SELECT * FROM Transaction t WHERE t.user.id = :userId")
+    List<Transaction> findByUserId(@Param("userId") Long userId);
+
+    @Query ("SELECT * FROM Transaction t WHERE t.user.id = :userId AND t.type = :type")
+    List<Transaction> findByType(@Param("userId") Long userId, @Param("type") String type);
+
+    @Query("SELECT * FROM Transaction t WHERE t.user.id = :userId AND YEAR(t.date) = :year AND MONTH(t.date) = :month")
+    List<Transaction> findByYearAndMonth(@Param("userId") Long userId, @Param("year") int year, @Param("month") int month);
 }
