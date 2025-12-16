ce9e0f428f1a362227d9cd538708bd49c450022a
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-16 18:38:20 -0300

    docs: Add OpenAPI docs and initial README structure
    Closes: #21

--- src/main/java/com/github/arthurscarpin/balancee/domain/user/controller/UserController.java
+++ src/main/java/com/github/arthurscarpin/balancee/domain/user/controller/UserController.java
@@ -4,6 +4,9 @@ import com.github.arthurscarpin.balancee.domain.user.dto.UserRequestDTO;
 import com.github.arthurscarpin.balancee.domain.user.dto.UserResponseDTO;
 import com.github.arthurscarpin.balancee.domain.user.service.UserService;
 import com.github.arthurscarpin.balancee.exception.BusinessException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +24,11 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new user.", description = "Creates a new user with the provided details.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input data.")
+    })
     public ResponseEntity<?> createPostUser(@RequestBody UserRequestDTO user) {
         try {
             UserResponseDTO userCreated = service.create(user);
@@ -33,12 +41,21 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "List all users.", description = "Retrieves a list of all registered users.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of users retrieved successfully."),
+    })
     public ResponseEntity<?> findAllGetUsers() {
         List<UserResponseDTO> users = service.findAll();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get user by ID.", description = "Retrieves a user by its unique ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User by ID."),
+            @ApiResponse(responseCode = "404", description = "User not found.")
+    })
     public ResponseEntity<?> findByIdGetUser(@PathVariable Long id) {
         try {
             UserResponseDTO user = service.findById(id);
@@ -50,6 +67,11 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update user by ID.", description = "Updates the details of an existing user by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User updated successfully."),
+            @ApiResponse(responseCode = "404", description = "User not found.")
+    })
     public ResponseEntity<?> updateByIdPutUser(@PathVariable Long id, @RequestBody UserRequestDTO user) {
         try {
             UserResponseDTO userUpdated = service.updateById(id, user);
@@ -61,6 +83,11 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete user by ID.", description = "Deletes a user by its unique ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully."),
+            @ApiResponse(responseCode = "404", description = "User not found.")
+    })
     public ResponseEntity<?> deleteByIdDeleteUser(@PathVariable Long id) {
         try {
             service.deleteById(id);
