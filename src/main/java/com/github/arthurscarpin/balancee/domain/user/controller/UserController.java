6046bef6b55c0a96386b998117ad265b8676759c
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-15 22:43:33 -0300

    feat: Add the User Controller and adjust the Service methods
    Closes #12

--- src/main/java/com/github/arthurscarpin/balancee/domain/user/controller/UserController.java
+++ src/main/java/com/github/arthurscarpin/balancee/domain/user/controller/UserController.java
@@ -1,4 +1,73 @@
 package com.github.arthurscarpin.balancee.domain.user.controller;
 
+import com.github.arthurscarpin.balancee.domain.user.model.User;
+import com.github.arthurscarpin.balancee.domain.user.service.UserService;
+import com.github.arthurscarpin.balancee.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createPostUser(@RequestBody User user) {
+        try {
+            User userCreated = service.create(user);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(userCreated);
+        } catch (BusinessException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllGetUsers() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findByIdGetUser(@PathVariable Long id) {
+        try {
+            User user = service.findById(id);
+            return ResponseEntity.ok(user);
+        } catch (BusinessException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateByIdPutUser(@PathVariable Long id, @RequestBody User user) {
+        try {
+            User userUpdated = service.updateById(id, user);
+            return ResponseEntity.ok(userUpdated);
+        } catch (BusinessException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteByIdDeleteUser(@PathVariable Long id) {
+        try {
+            service.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .build();
+        } catch (BusinessException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }
+    }
 }
