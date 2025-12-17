ec1609039d1d6ae7de58953cadebbf53fe326dda
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-16 22:49:57 -0300

    fix: correct bug DTO

--- src/main/java/com/github/arthurscarpin/balancee/domain/user/dto/UserRequestDTO.java
+++ src/main/java/com/github/arthurscarpin/balancee/domain/user/dto/UserRequestDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserRequestDTO(
-        Long id,
         @NotBlank String name,
         @NotBlank @Email String email
 ) {
