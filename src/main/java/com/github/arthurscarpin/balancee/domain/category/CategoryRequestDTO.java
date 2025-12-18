85ad916ec85ddb94fe54b684424a5a460c7dedb8
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-18 17:50:29 -0300

    fix: Remove unused id attribute

--- src/main/java/com/github/arthurscarpin/balancee/domain/category/CategoryRequestDTO.java
+++ src/main/java/com/github/arthurscarpin/balancee/domain/category/CategoryRequestDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CategoryRequestDTO(
-        Long id,
         @NotBlank String name,
         @NotNull CategoryType type
 ) {
